4ffb2aa2fcc734e7ba87d081b5bb8deedf2af50f
Natalisp <nathalipat@gmail.com>
2017-01-13 13:17:05 -0500

    modal style and ajax

--- app/assets/javascripts/components/_button.js.jsx
+++ app/assets/javascripts/components/_button.js.jsx
@@ -3,25 +3,46 @@ class Button extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      modalOpen: false
+      modalOpen: false,
+      modalContent: ''
     }
   }
+
+  close(e) {
+    e.preventDefault();
+    this.setState({ modalOpen: false });
+ }
+
+  open(e) {
+    e.preventDefault();
+    this.setState({ modalOpen: true });
+    $.getJSON('/api/v1/facts.json', (response) => {
+      const i = response.randomElement();
+      // console.log(response[0].content)
+      // debugger
+      this.setState({
+        modalContent: i.content
+      })
+    });
+    }
 
   toggleModal(e) {
     e.preventDefault();
     this.setState({modalOpen: !this.state.modalOpen})
-    console.log('Hola')
   }
-
 
   render() {
     return (
       <div>
-      <a href='#' onClick={this.toggleModal.bind(this)}>{this.state.modalOpen ? 'Close Me' : 'Open Me'}</a>
-      <Modal open={this.state.modalOpen} >
-        <p>Waaaaaaah</p>
+      <a href='#' onClick={this.open.bind(this)}>{this.state.modalOpen ? 'How about...' : 'Did you know...'}</a>
+      <Modal open={this.state.modalOpen} onHide={this.close.bind(this)} >
+        <p>{this.state.modalContent}</p>
       </Modal>
       </div>
     )
   }
 }
+
+Array.prototype.randomElement = function () {
+      return this[Math.floor(Math.random() * this.length)]
+  }
--- app/assets/javascripts/components/_modal.js.jsx
+++ app/assets/javascripts/components/_modal.js.jsx
@@ -1,4 +1,5 @@
 class Modal extends React.Component {
+
   render() {
     const styles = {
       modalOpen: {
@@ -7,21 +8,36 @@ class Modal extends React.Component {
         margin: '2rem',
         padding: '2rem',
         transform: 'translate Y(0%)',
-        transition: 'all 1s ease'
+        transition: 'all 1s ease',
+        textAlign: 'center',
+        width: '800px',
+        maxWidth: '100%'
       },
       modalClosed: {
         display: 'none',
         transform: 'translate Y(-100%)',
         transition: 'all 1s ease'
+      },
+    closeButton: {
+       color: '#fff',
+       textDecoration: 'none',
+       display: 'inline-block',
+       padding: '5px 5px',
+       borderRadius: '5px',
+       backgroundColor: '#E74C3C'
       }
-
     }
 
     let modalStyles = this.props.open ? styles.modalOpen : styles.modalClosed;
 
 
     return (
-      <div style={modalStyles}>{this.props.children}</div>
+      <div style={modalStyles} >
+         {this.props.children}
+          <div style={styles.closeButton}>
+            <a href="#" onClick={this.props.onHide}>Close</a>
+          </div>
+      </div>
     )
   }
 }
